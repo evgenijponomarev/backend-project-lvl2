301366e4fa87a0b652c45ea8ac618b15156548fe
evgeniy1801 <evgeniy1801@gmail.com>
2021-03-03 18:13:04 +0700

    Add format option to help

--- src/cli.js
+++ src/cli.js
@@ -1,30 +1,44 @@
 import fs from 'fs';
+
 import { Command } from 'commander/esm.mjs';
 
 export default class Cli {
-  static getAppVersion() {
+  static getPackageVersion() {
     const { version } = JSON.parse(fs.readFileSync('package.json', 'utf8'));
 
     return version;
   }
 
   constructor() {
-    const version = this.constructor.getAppVersion();
+    const version = this.constructor.getPackageVersion();
 
     this.program = new Command();
 
     this.setVersion(version);
+    this.setHelpCommand();
+    this.setDescription();
     this.setArguments();
+    this.setOptions();
   }
 
   setVersion(version) {
     this.program.version(version, '-V, --version');
   }
 
+  setHelpCommand() {
+    this.program.helpOption('-h, --help', 'output usage information');
+  }
+
   setArguments() {
-    this.program
-      .arguments('<filepath1> <filepath2>')
-      .description('Compares two configuration files and shows a difference.');
+    this.program.arguments('<filepath1> <filepath2>');
+  }
+
+  setDescription() {
+    this.program.description('Compares two configuration files and shows a difference.');
+  }
+
+  setOptions() {
+    this.program.option('-f, --format', 'output format');
   }
 
   init() {
