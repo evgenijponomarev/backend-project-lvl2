01e0f9f9d0bfca5dcb408a7c6054a8f6dbae6301
evgeniy1801 <evgeniy1801@gmail.com>
2021-03-05 19:17:15 +0700

    Add fixtures for tests

---
+++ __fixtures__/sample-objects.js
@@ -0,0 +1,31 @@
+export const obj1 = {
+  field1: 'value1',
+  field2: 'value2',
+  field3: 'value3',
+};
+
+export const obj2 = {
+  field1: 'value1',
+  field2: 'value2 changed',
+  field4: 'value4',
+};
+
+export const obj3 = {
+  field1: undefined,
+  field2: 'value2',
+  field3: undefined,
+};
+
+export const obj4 = {
+  field1: 'value1',
+  field2: undefined,
+  field4: undefined,
+};
+
+export const obj5 = {
+  toString: 'not function',
+};
+
+export const obj6 = {
+  field1: 'value1',
+};
